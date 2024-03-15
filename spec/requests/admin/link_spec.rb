manyfold3d/manyfold

test: additional admin view spec files

spec/requests/admin/link_spec.rb
@@ -1,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe "Admin::Link" do
+  it "is inaccessible to normal user" do
+    sign_in create(:user, admin: false)
+    get "/admin/links"
+    expect(response).to have_http_status(:unauthorized)
+  end
+
+  context "with admin permission" do
+    before do
+      sign_in create(:user, admin: true)
+    end
+
+    it "is accessible" do
+      get "/admin/links"
+      expect(response).to have_http_status(:success)
+    end
+
+    it "is inaccessible in demo mode" do
+      allow(SiteSettings).to receive(:demo_mode?).and_return(true)
+      expect { get("/admin/links") }.to raise_error(Pundit::NotAuthorizedError)
+    end
+  end
+end

spec/requests/admin/problem_spec.rb
@@ -1,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe "Admin::Problem" do
+  it "is inaccessible to normal user" do
+    sign_in create(:user, admin: false)
+    get "/admin/problems"
+    expect(response).to have_http_status(:unauthorized)
+  end
+
+  context "with admin permission" do
+    before do
+      sign_in create(:user, admin: true)
+    end
+
+    it "is accessible" do
+      get "/admin/problems"
+      expect(response).to have_http_status(:success)
+    end
+
+    it "is inaccessible in demo mode" do
+      allow(SiteSettings).to receive(:demo_mode?).and_return(true)
+      expect { get("/admin/problems") }.to raise_error(Pundit::NotAuthorizedError)
+    end
+  end
+end

WaterSwallow/Many_Fold

spec/requests/admin/link_spec.rb
@@ -1,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe "Admin::Links" do
+  it "is inaccessible to normal user" do
+    sign_in create(:user, admin: false)
+    get "/admin/links"
+    expect(response).to have_http_status(:unauthorized)
+  end
+
+  context "with admin permission" do
+    before do
+      sign_in create(:user, admin: true)
+    end
+
+    it "is accessible" do
+      get "/admin/links"
+      expect(response).to have_http_status(:success)
+    end
+
+    it "is inaccessible in demo mode" do
+      allow(SiteSettings).to receive(:demo_mode?).and_return(true)
+      expect { get("/admin/links") }.to raise_error(Pundit::NotAuthorizedError)
+    end
+  end
+end

spec/requests/admin/problem_spec.rb
@@ -1,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe "Admin::Problems" do
+  it "is inaccessible to normal user" do
+    sign_in create(:user, admin: false)
+    get "/admin/problems"
+    expect(response).to have_http_status(:unauthorized)
+  end
+
+  context "with admin permission" do
+    before do
+      sign_in create(:user, admin: true)
+    end
+
+    it "is accessible" do
+      get "/admin/problems"
+      expect(response).to have_http_status(:success)
+    end
+
+    it "is inaccessible in demo mode" do
+      allow(SiteSettings).to receive(:demo_mode?).and_return(true)
+      expect { get("/admin/problems") }.to raise_error(Pundit::NotAuthorizedError)
+    end
+  end
+end